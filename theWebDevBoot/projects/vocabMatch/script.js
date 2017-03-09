200b9b0ed0a2a2417fb4ab5737e207e4130c53fb
cemsity <cemsity@gmail.com>
2017-03-09 17:53:06 +0100

    finished with the Vocab Match game; now it is time to refactor and make betterto goals

--- theWebDevBoot/projects/vocabMatch/script.js
+++ theWebDevBoot/projects/vocabMatch/script.js
@@ -23,10 +23,75 @@ var diction = [
 
 /// data vars
 var numSquares = 6;
-var pickedWord = "";
+var pickedWord = '';
+var words = [];
 var score = 0;
+var language = 0;
 // element vars
-var wordsDiv = $(".words");
-var chosenDiv = $("#chosen");
+var scoreDis = $("#score");
+var squaresBtn = $(".square");
+var wordsDis = $(".words");
+var chosenDis = $(".chosen");
 var resetBtn = $("#reset");
-var
+var messageDis = $("#message");
+
+
+// hook up squares button
+squaresBtn.on('click', function() {
+   var selected = $(this).children().text();
+   console.log(selected);
+   if (selected === pickedWord[1]){
+      score++;
+      messageDis.text("Bon Travail");
+      scoreDis.text(score);
+      setup();
+   } else {
+      messageDis.text("Mal Chance");
+      score--;
+      scoreDis.text(score);
+      setup();
+   }
+});
+setup();
+resetBtn.on('click', function() {
+    setup();
+});
+
+function setup() {
+    words = pickWords(numSquares);
+    pickedWord = chooseWord(words);
+    display();
+
+}
+
+function display() {
+    wordsDis.each(function(index) {
+        $(this).text(words[index][1]);
+
+    })
+    chosenDis.text(pickedWord[language]);
+}
+
+function chooseWord(list) {
+    return list[randNum(words.length, 0)]
+}
+
+function pickWords(num) {
+    var arr = [];
+    var newWordPair = [];
+    while (arr.length < num) {
+        newWordPair = randWord();
+        if (arr.indexOf(newWordPair) == -1) {
+            arr.push(newWordPair);
+        }
+    }
+    return arr;
+}
+
+function randWord() {
+    return diction[randNum(diction.length, 0)];
+}
+
+function randNum(num, start) {
+    return Math.floor(Math.random() * num + start);
+}
